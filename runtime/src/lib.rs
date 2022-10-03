f9ca8262dcba05f32e53523378e5cdfcb49330b0
Bader Youssef <ibnbassem@gmail.com>
2022-10-03 19:22:39 -0400

    modified for uke pallet

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -44,7 +44,7 @@ pub use sp_runtime::BuildStorage;
 pub use sp_runtime::{Perbill, Permill};
 
 /// Import the template pallet.
-pub use pallet_template;
+pub use pallet_uke;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -266,8 +266,11 @@ impl pallet_sudo::Config for Runtime {
 }
 
 /// Configure the pallet-template in pallets/template.
-impl pallet_template::Config for Runtime {
+impl pallet_uke::Config for Runtime {
 	type Event = Event;
+	type MaxUsernameLength = ConstU32<16>;
+	type MaxConvoIdLength = ConstU32<64>;
+	type WeightInfo = pallet_uke::weights::PalletWeight<Runtime>;
 }
 
 // Create the runtime by composing the FRAME pallets that were previously configured.
@@ -287,7 +290,7 @@ construct_runtime!(
 		TransactionPayment: pallet_transaction_payment,
 		Sudo: pallet_sudo,
 		// Include the custom logic from the pallet-template in the runtime.
-		TemplateModule: pallet_template,
+		Uke: pallet_uke,
 	}
 );
 
@@ -332,7 +335,7 @@ mod benches {
 		[frame_system, SystemBench::<Runtime>]
 		[pallet_balances, Balances]
 		[pallet_timestamp, Timestamp]
-		[pallet_template, TemplateModule]
+		[pallet_uke, Uke]
 	);
 }
 
